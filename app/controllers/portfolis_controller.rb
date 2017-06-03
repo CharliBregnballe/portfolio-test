23392575eb6704bf34faa32e7f36c1d6fca6428c
Charli Bregnballe <charli@kompjuter.dk>
2017-06-03 15:31:12 +0200

    Added new and create action for portoli

--- app/controllers/portfolis_controller.rb
+++ app/controllers/portfolis_controller.rb
@@ -3,4 +3,21 @@ class PortfolisController < ApplicationController
     @portfoli_items = Portfoli.all
   end
 
+  def new
+    @portfoli_item = Portfoli.new
+  end
+
+  def create
+    @portfoli_item = Portfoli.new(params.require(:portfoli).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfoli_item.save
+        format.html { redirect_to portfolis_path, notice: 'portfoli_item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfoli_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfoli_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
