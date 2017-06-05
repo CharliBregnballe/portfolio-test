f4f3793dc45e2fc6ca1ef61f014b9134966a87d1
Charli Bregnballe <charli@kompjuter.dk>
2017-06-05 21:52:50 +0200

    Added a custom route to the portolis show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolis
+  resources :portfolis, except: [:show]
+  get 'portfoli/:id', to: 'portfolis#show', as: 'portfoli_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
