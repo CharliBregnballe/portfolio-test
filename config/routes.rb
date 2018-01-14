20f51c2f37c3439c1ac37bf455abf05b013cd95e
Charli Bregnballe <charli@kompjuter.dk>
2018-01-14 22:07:35 +0100

    Customized registration, login and lougout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolis, except: [:show]
   get 'angular-items', to: 'portfolis#angular'
   get 'portfoli/:id', to: 'portfolis#show', as: 'portfoli_show'
