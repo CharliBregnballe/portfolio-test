91570eef86932f50f5c962d06f5c4ff56d607ab2
Charli Bregnballe <charli@kompjuter.dk>
2018-02-16 00:14:24 +0100

    integrated copyright stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = CharlisViewTool::Renderer.copyright 'Charli Bregnballe', 'All rights reserved'
+  end
+end
+
+
+module CharlisViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
